3a876b3ccd4044535a80c7879ff64e24ccb42a97
bhupesh-waglogy <bhupesh.waglogy@gmail.com>
2024-12-01 09:06:55 +0530

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // Import the router
-
-const app = createApp(App);
-
-app.use(router); // Use the router in the app
-app.mount('#app');
